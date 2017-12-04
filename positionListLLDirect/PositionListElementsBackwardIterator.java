5957b27421a056a738f68342cd89b3d45f0e7f6c
javierbustillo <javier.bustillo@upr.edu>
2017-12-04 17:35:10 -0400

    Finished positionBackwardIterator

--- positionListLLDirect/PositionListElementsBackwardIterator.java
+++ positionListLLDirect/PositionListElementsBackwardIterator.java
@@ -1,23 +1,46 @@
 package positionListLLDirect;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import exceptionClasses.EmptyListException;
+import positionInterfaces.Position;
+import positionInterfaces.PositionList;
 
 public class PositionListElementsBackwardIterator<E> implements Iterator<E> {
 
-	public PositionListElementsBackwardIterator() {
+	private Position<E> current; 
+	private PositionList<E> theList;
+	
+	public PositionListElementsBackwardIterator(PositionList<E> list) {
 		// TODO Auto-generated constructor stub
+		theList = list; 
+		try { 
+			current = theList.last(); 
+		}
+	    catch (EmptyListException e) { 
+	    	current = null; 
+	    }
 	}
 
 	@Override
 	public boolean hasNext() {
 		// TODO Auto-generated method stub
-		return false;
+		return current!=null;
 	}
 
 	@Override
 	public E next() {
-		// TODO Auto-generated method stub
-		return null;
+		if (!hasNext()) 
+			throw new NoSuchElementException("Iterator has past the end.");
+		Position<E> ptr = current; 
+		try { 
+			current = theList.prev(current); 
+		}
+		catch (Exception e) { 
+			current = null; 
+		}
+		return ptr.element();
 	}
 
 }
